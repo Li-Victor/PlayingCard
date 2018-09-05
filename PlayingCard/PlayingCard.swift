7201763340765b329c0609580d6b794b21d6b52a
Victor Li <victorli5674@yahoo.com>
2018-09-04 22:53:50 -0700

    playing card with gestures

--- PlayingCard/PlayingCard.swift
+++ PlayingCard/PlayingCard.swift
@@ -10,34 +10,35 @@ import Foundation
 
 struct PlayingCard: CustomStringConvertible {
     var description: String {
-        return "\(rank) \(suit)"
+        return "\(rank)\(suit)"
     }
     
     var suit: Suit
     var rank: Rank
     
-    enum Suit: String {
+    enum Suit: String, CustomStringConvertible {
         
         case spades = "♠️"
         case hearts = "♥️"
         case clubs = "♣️"
         case diamonds = "♦️"
         static var all = [Suit.spades, .hearts, .diamonds, .clubs]
+        var description: String { return rawValue }
     }
     
-    enum Rank {
+    enum Rank: CustomStringConvertible {
         case ace
         case face(String)
         case numeric(Int)
         
         var order: Int {
             switch self {
-                case .ace: return 1
-                case .numeric(let pips): return pips
-                case .face(let kind) where kind == "J": return 11
-                case .face(let kind) where kind == "Q": return 12
-                case .face(let kind) where kind == "K": return 13
-                default: return 0
+            case .ace: return 1
+            case .numeric(let pips): return pips
+            case .face(let kind) where kind == "J": return 11
+            case .face(let kind) where kind == "Q": return 12
+            case .face(let kind) where kind == "K": return 13
+            default: return 0
             }
         }
         static var all: [Rank] {
@@ -48,6 +49,14 @@ struct PlayingCard: CustomStringConvertible {
             allRanks += [.face("J"), .face("Q"), .face("K" )]
             return allRanks
         }
+        
+        var description: String {
+            switch self {
+            case .ace:
+                return "A"
+            case .numeric(let pips): return String(pips)
+            case .face(let kind): return kind
+            }
+        }
     }
-    
 }
